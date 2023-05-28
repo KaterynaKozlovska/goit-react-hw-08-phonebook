3fa1faef6a151f769d3cc1c51dc3659e581a0fd7
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-28 23:20:35 +0300

    update app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,36 +3,38 @@ import { ContactList } from '../List/List';
 import { Filter } from '../Filter/Filter';
 import css from './App.module.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import {
   selectContactsItems,
   selectIsLoading,
   selectError,
 } from '../../redux/contacts/selectors';
-import { fetchContacts } from '../../redux/contacts/operations';
+import { userRefresh } from '../../redux/contacts/operations';
 import { toast } from 'react-toastify';
 import { TailSpin } from 'react-loader-spinner';
+import { useFetchingCurrentUser } from 'hooks';
 
 function App() {
   const contactsItems = useSelector(selectContactsItems);
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+  const isFetchingCurrentUser = useFetchingCurrentUser();
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(userRefresh());
   }, [dispatch]);
 
-  useEffect(() => {
-    if (error === 'ERR_BAD_REQUEST') {
-      toast.error('There are some problems! Try again later.');
-      return;
-    }
-    if (error) {
-      toast.error(error);
-    }
-  }, [error]);
+  // useEffect(() => {
+  //   if (error === 'ERR_BAD_REQUEST') {
+  //     toast.error('There are some problems! Try again later.');
+  //     return;
+  //   }
+  //   if (error) {
+  //     toast.error(error);
+  //   }
+  // }, [error]);
 
   return (
     <div className={css.container}>
