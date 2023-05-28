14dcf982c2dfafaf0ccce835c31c6e24c9e388ba
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-28 23:36:24 +0300

    Create contactSchema.js

---
+++ src/schemas/contactSchema.js
@@ -0,0 +1,23 @@
+import * as yup from 'yup';
+
+export const contactFormSchema = yup.object().shape({
+  name: yup
+    .string()
+    .trim()
+    .max(64)
+    .required('Name is required')
+    .matches(nameRegex, {
+      message:
+        "Invalid name. Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan.",
+    }),
+
+  number: yup
+    .string()
+    .trim()
+    .required('Number is required')
+    .min(5)
+    .matches(numberRegex, {
+      message:
+        'Invalid number. Phone number must be digits and can contain spaces, dashes, parentheses and can start with +.',
+    }),
+});
