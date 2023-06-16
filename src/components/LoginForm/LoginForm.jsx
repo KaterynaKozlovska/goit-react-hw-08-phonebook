5092babf052672181d15ec6e8fc6dadabdecc704
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-06-16 22:32:03 +0300

    delete reset form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -25,15 +25,13 @@ export const LoginForm = () => {
   const dispatch = useDispatch();
   const [passwordShown, setPasswordShown] = useState(false);
 
-  const handleSubmit = (values, { resetForm }) => {
+  const handleSubmit = values => {
     dispatch(
       logIn({
         email: values.email,
         password: values.password,
       })
     );
-
-    resetForm();
   };
 
   const togglePassword = () => setPasswordShown(!passwordShown);
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -25,7 +25,7 @@ export const RegisterForm = () => {
   const dispatch = useDispatch();
   const [passwordShown, setPasswordShown] = useState(false);
 
-  const handleSubmit = (values, { resetForm }) => {
+  const handleSubmit = values => {
     dispatch(
       register({
         name: values.username,
@@ -33,8 +33,6 @@ export const RegisterForm = () => {
         password: values.password,
       })
     );
-
-    resetForm();
   };
 
   const togglePassword = () => setPasswordShown(!passwordShown);
