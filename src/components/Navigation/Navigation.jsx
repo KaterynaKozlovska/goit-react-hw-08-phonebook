4cddaff344ab3adcbff4b082295c4b0bfba896ab
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-06-25 19:51:46 +0300

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,7 @@
 import { NavList, NavItemLink } from './Navigation.styled';
 import { useAuth } from 'hooks/useAuth';
+import { AuthNav } from '../AuthNav/AuthNav';
+import { UserMenu } from '../UserMenu/UserMenu';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
@@ -9,6 +11,7 @@ export const Navigation = () => {
       <NavList>
         <NavItemLink to={'/'}>Home</NavItemLink>
         {isLoggedIn && <NavItemLink to={'/contacts'}>Contacts</NavItemLink>}
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
       </NavList>
     </nav>
   );
